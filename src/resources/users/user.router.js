08e27e4ab26fef9e1365f41a7de3a4db3300c7bb
Alex G <archon.san@gmail.com>
2020-04-05 11:44:37 +0300

    after merge tests

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,56 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+  if (users.length < 1) {
+    res.status(404).send('Users not found');
+  } else {
+    res.json(users.map(User.toResponse));
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  const user = new User(req.body);
+  if (!User.toResponse(user)) {
+    res.status(400).send('Bad request');
+  } else {
+    const users = await usersService.getAll();
+    usersService.pushNew(user, users);
+    res.json(User.toResponse(user));
+  }
+});
+
+router.route('/:id').get(async (req, res) => {
+  const users = await usersService.getAll();
+  const user = usersService.getById(users, req.params.id);
+  if (!User.toResponse(user)) {
+    res.status(404).send('User not found');
+  } else {
+    res.json(User.toResponse(user));
+  }
+});
+
+router.route('/:id').put(async (req, res) => {
+  const users = await usersService.getAll();
+  const user = usersService.getById(users, req.params.id);
+
+  if (!User.toResponse(user)) {
+    res.status(404).send('User not found');
+  } else {
+    await usersService.update(users, user, req.body);
+    res.json(User.toResponse(user));
+  }
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const users = await usersService.getAll();
+  const user = usersService.getById(users, req.params.id);
+
+  if (!User.toResponse(user)) {
+    res.status(404).send('User not found');
+  } else {
+    await usersService.remove(users, req.params.id);
+    res.status(204).send('The user has been deleted');
+  }
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,10 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+module.exports = {
+  getAll,
+  pushNew: usersRepo.pushNew,
+  getById: usersRepo.getById,
+  update: usersRepo.update,
+  remove: usersRepo.remove
+};
